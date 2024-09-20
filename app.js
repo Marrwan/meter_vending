d1f23819b7341cd99576f2c95db422aa0c1ff76f
Abdulbasit Alabi <abdulbasitdamilola6@gmail.com>
2024-09-20 10:51:34 +0100

    code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: '*',
+    origin: 'https://meter-vending-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
